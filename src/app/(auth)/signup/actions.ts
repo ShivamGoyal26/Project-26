af79c725dbc614410dbdcd636aee9745a891fe58
shivamgoyal26 <officialshivamgoyal@gmail.com>
2024-08-25 23:58:07 +0530

    signup server action developed

---
+++ src/app/(auth)/signup/actions.ts
@@ -0,0 +1,90 @@
+"use server";
+
+import { hash } from "@node-rs/argon2";
+import { signUpSchema, SignUpValues } from "@/lib/validation";
+import { generateIdFromEntropySize } from "lucia";
+import prisma from "@/lib/prisma";
+import { lucia } from "@/auth";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import { isRedirectError } from "next/dist/client/components/redirect";
+
+export async function signUp(
+  credentials: SignUpValues,
+): Promise<{ error: string }> {
+  try {
+    const parseResult = signUpSchema.safeParse(credentials);
+
+    if (!parseResult.success) {
+      return {
+        error: parseResult.error.errors.map((e) => e.message).join(", "),
+      };
+    }
+
+    const { username, email, password } = parseResult.data;
+
+    const hashedPassword = await hash(password, {
+      memoryCost: 19456,
+      timeCost: 2,
+      outputLen: 32,
+      parallelism: 1,
+    });
+    const userId = generateIdFromEntropySize(10);
+
+    const existingUsername = await prisma.user.findFirst({
+      where: {
+        username: {
+          equals: username,
+          mode: "insensitive",
+        },
+      },
+    });
+
+    if (existingUsername) {
+      return {
+        error: "Username already taken",
+      };
+    }
+
+    const existingEmail = await prisma.user.findFirst({
+      where: {
+        email: {
+          equals: email,
+          mode: "insensitive",
+        },
+      },
+    });
+
+    if (existingEmail) {
+      return {
+        error: "Email already taken",
+      };
+    }
+
+    await prisma.user.create({
+      data: {
+        id: userId,
+        username,
+        email,
+        displayName: username,
+        passwordHash: hashedPassword,
+      },
+    });
+
+    const session = await lucia.createSession(userId, {});
+    const sessionCookie = await lucia.createSessionCookie(session.id);
+    cookies().set(
+      sessionCookie.name,
+      sessionCookie.value,
+      sessionCookie.attributes,
+    );
+
+    return redirect("/");
+  } catch (error) {
+    if (isRedirectError(error)) throw error; // this error will be catched by same catch block means this block only
+    console.log(error);
+    return {
+      error: "something went wrong, please try again",
+    };
+  }
+}
